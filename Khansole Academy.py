0417fbec27473bb7bbb3b29dff1ab72427df1fee
anukshim <134408923+anukshim@users.noreply.github.com>
2023-05-23 20:37:16 +0530

    Create Khansole Academy.py
    A program that helps other people learn addition! The program randomly generates a simple addition problem for the user, reads in the answer from the user, and then checks to see if they got it right or wrong.

---
+++ Khansole Academy.py
@@ -0,0 +1,17 @@
+import random
+
+def main():
+    print("Khansole Academy")
+    num1= random.randint(10,99)
+    num2= random.randint(10,99)
+    sum=num1+num2
+    print ("What is " + str(num1) + "+" + str(num2))
+    answer=int(input("Your answer:"))
+    if answer==sum:
+        print("You are correct. Good Job!")
+    else:
+        print("Sorry. This is incorrect. The expected answer is "  +  str(sum) )
+    
+    
+if __name__ == '__main__':
+    main()
