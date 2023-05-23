71dc66a9eaad027bd51599634b6448ea6492c2c8
anukshim <134408923+anukshim@users.noreply.github.com>
2023-05-23 21:09:44 +0530

    Create 8. LiftOff.py

---
+++ 8. LiftOff.py
@@ -0,0 +1,17 @@
+"""
+Program: Liftoff
+--------------------
+Countdown from 10 to 1 and then print Liftoff!
+You can view the result here: https://codeinplace.stanford.edu/cip3/share/8BXmC76S2iWiPaT79mCQ 
+"""
+
+def main():
+    for i in range (10,0,-1):
+        print(i)
+    print("Liftoff!")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
